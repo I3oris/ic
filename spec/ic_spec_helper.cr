2b88532a456c1c6490f132236531993fb194debc
I3oris <boris31.resch@gmail.com>
2022-06-05 02:42:41 +0200

    Write spec for `History`.

--- spec/ic_spec_helper.cr
+++ spec/ic_spec_helper.cr
@@ -15,4 +15,32 @@ module IC::Spec
     receiver.try(&.type).to_s.should eq type
     scope.to_s.should eq with_scope
   end
+
+  def self.history_entries
+    [
+      [%(puts "Hello World")],
+      [%(i = 0)],
+      [
+        %(while i < 10),
+        %(  puts i),
+        %(  i += 1),
+        %(end),
+      ],
+    ]
+  end
+
+  def self.empty_history
+    IC::ReplInterface::History.new
+  end
+
+  def self.history
+    history = IC::ReplInterface::History.new
+    self.history_entries.each { |e| history << e }
+    history
+  end
+
+  def self.verify_history(history, entries, index)
+    history.@history.should eq entries
+    history.@index.should eq index
+  end
 end
